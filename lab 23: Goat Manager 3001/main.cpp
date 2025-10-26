d2065a7d424d3dce737b86bc7a346ef90c8dcfc8
Guo An Wang <you@example.com>
2025-10-25 21:45:15 -0700

    Initial commit

---
+++ lab 23: Goat Manager 3001/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  COMSC-210 | lab 23: Goat Manager 3001 | Guo An Wang
+//  IDE: Xcode
+//  Created by Guo An Wang on 10/25/25.
+//
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <list>
+#include "Goat.h"
+using namespace std;
+
+const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
+
+//function prototypes
+int select_goat(list<Goat> trip);
+void delete_goat(list<Goat> &trip);
+void add_goat(list<Goat> &trip, string [], string []);
+void display_trip(list<Goat> trip);
+int main_menu();
+
+int main() {
+    srand(time(0));
+    bool again;
+
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int i = 0;
+    while (fin >> names[i++]);
+    fin.close();
+    ifstream fin1("colors.txt");
+    string colors[SZ_COLORS];
+    i = 0;
+    while (fin1 >> colors[i++]);
+    fin1.close();
+    
+    return 0;
+}
